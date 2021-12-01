797b9898bb821a114c4f75f61e271fd97efb6787
mgdonato23 <mg.donato23@gmail.com>
2021-12-01 15:28:10 -0300

    Implementaçao de validadores nos campos e inclusão do método de limpeza dos campos após o click

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,17 +21,38 @@ export class HomeComponent implements OnInit {
     private cadastroUsuariosService: CadastroUsuariosService
   ) {
     this.form = new FormGroup({
-      Email: new FormControl(''),
-      Nome: new FormControl(''),
-      CPF: new FormControl(''),
-      DataNascimento: new FormControl(''),
-      Endereco: new FormControl(''),
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Nome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      CPF: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/
+        ),
+      ]),
+      DataNascimento: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      Endereco: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
     });
   }
 
   ngOnInit(): void {}
 
   salvarRegistro(): void {
+    this.form.updateValueAndValidity();
+    this.form.markAllAsTouched();
+
+    if (this.form.invalid) {
+      return;
+    }
+
     const dadosRequisicao: UsuarioModel = {
       Email: this.form.value.Email,
       Nome: this.form.value.Nome,
@@ -45,6 +66,7 @@ export class HomeComponent implements OnInit {
       .postCadastro(dadosRequisicao)
       .subscribe((dados) => {
         console.log(dados);
+        this.form.reset();
       });
   }
 }
